8490b1da55b293cc8537baf1dc0e1ad67c251760
Sidorov Maksim <maxx11167@gmail.com>
2023-07-01 17:33:19 +0300

    feat: create about page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import { MainPage } from './components/pages/main/mainPage'
 import { CatalogPage } from './components/pages/catalog/catalogPage'
+import { AboutPage } from './components/pages/about/aboutPage'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="shoes_shop/" element={<MainPage />}/>
           <Route path="shoes_shop/catalog.html" element={<CatalogPage />}/>
+          <Route path="shoes_shop/about.html" element={<AboutPage />}/>
         </Routes>
     </>
   )
