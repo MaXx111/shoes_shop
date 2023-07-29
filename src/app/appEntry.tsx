313fbace7e57212511cef3c86666a8c8dce1ce55
Sidorov Maksim <maxx11167@gmail.com>
2023-07-29 17:52:43 +0300

    feat: add redux persist

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -3,12 +3,13 @@ import { router } from './appRouter'
 import { RouterProvider } from 'react-router-dom'
 import { Provider as ReduxProvider } from 'react-redux'
 import './App.css'
-import { setupStore } from './appStore'
-
-const appStore = setupStore()
+import store, { persistor } from './appStore'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <ReduxProvider store={appStore}>
-      <RouterProvider router={router} fallbackElement={<p>Loading...</p>}/>
+    <ReduxProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} fallbackElement={<p>Loading...</p>}/>
+      </PersistGate>
     </ReduxProvider>
 )
--- src/app/appStore.ts
+++ src/app/appStore.ts
@@ -1,14 +1,41 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { rootReducer } from './rootReducer'
+import { persistStore, persistReducer, 
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER, } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const setupStore = () => {
+const setupStore = () => {
     return configureStore({
-        reducer: rootReducer
+        reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({
+                serializableCheck: {
+                    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+                },
+            }),
     })
 }
+
+const store = setupStore()
+
+export const persistor = persistStore(store)
+
+
 
 export type RootState = ReturnType<typeof rootReducer>
 export type AppStore = ReturnType<typeof setupStore>
 export type AppDispatch = AppStore['dispatch']
+
+export default store
